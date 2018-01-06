91a627c2e4d69cd968a3d8df8eb0bdfee1427f4f
Sam Slotsky <saxosamo@gmail.com>
2018-01-05 22:53:41 -0600

    Video 1.3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { Component, Fragment } from "react";
+import Home from "./Home";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h2>Hot reloading is amazing!</h2>
-      </div>
+      <Fragment><Home /><h1 key="heading">Is where the heart is?</h1></Fragment>
     );
   }
 }
---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+
+export default function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <h2>Hot reloading is amazing!</h2>
+    </div>
+  );
+}
