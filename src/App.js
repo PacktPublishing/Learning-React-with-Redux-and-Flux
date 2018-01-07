c7960f2f1b6b0a34f5c8acc86149fcd064d4dbad
Sam Slotsky <saxosamo@gmail.com>
2018-01-06 22:17:53 -0600

    Video 2.1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return <Home />;
+    return <Home message="Hot reloading is great!" />;
   }
 }
 
--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,14 @@ import React from "react";
 import { withLayout } from "./Layout";
 import { AppIntro } from "./styles";
 
-export function Home() {
+export function Home({ message }) {
   return (
     <React.Fragment>
       <AppIntro>
         To get started, edit <code>src/App.js</code> and save to reload.
       </AppIntro>
-      <h2>Hot reloading is amazing!</h2>
+      <h2>{message}</h2>
+      <button onClick={() => (message = "foo")}>Click me!</button>
     </React.Fragment>
   );
 }
